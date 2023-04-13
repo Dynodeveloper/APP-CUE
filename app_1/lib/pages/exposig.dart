1f0dbb1ff491b3b83a6decff86a77beb138c7018
dyno <cesardyno0207@gmail.com>
2023-04-13 16:30:31 -0500

    actualizacion plano ExpoSIG

--- app_1/lib/pages/exposig.dart
+++ app_1/lib/pages/exposig.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class ExpoSIG extends StatefulWidget {
   const ExpoSIG({Key? key}) : super(key: key);
 
@@ -7,9 +8,11 @@ class ExpoSIG extends StatefulWidget {
   _ExpoSIGState createState() => _ExpoSIGState();
 }
 
+//pagina de ExpoSIG
+
 class _ExpoSIGState extends State<ExpoSIG> {
-  double _scale = 1.0;
-  double _previousScale = 1.0;
+  final icono1 = Icons.location_on;
+  final icono2 = Icons.location_on;
 
   Widget _buildDialog(BuildContext context, String title, String description,
       String imageAsset) {
@@ -35,79 +38,174 @@ class _ExpoSIGState extends State<ExpoSIG> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onScaleStart: (details) {
-        setState(() {
-          _previousScale = _scale;
-        });
-      },
-      onScaleUpdate: (details) {
-        setState(() {
-          _scale = _previousScale * details.scale;
-        });
-      },
-      child: InteractiveViewer(
-        boundaryMargin: EdgeInsets.all(100),
-        minScale: 0.1,
-        maxScale: 2.0,
-        child: Stack(
-          children: [
-            // Widget de tu plano o mapa
-            Image(
+      child: Stack(
+        children: [
+          // Widget de tu plano o mapa
+          InteractiveViewer(
+            child: Image(
               image: AssetImage('assets/Plano.png'),
               fit: BoxFit.contain,
               width: double.infinity,
               height: double.infinity,
             ),
-            // Ventana emergente 1
-            Positioned(
-              left: 50,
-              top: 50,
-              child: GestureDetector(
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (_) => _buildDialog(
-                      context,
-                      'Título de la ventana emergente 1',
-                      'Descripción de la ventana emergente 1',
-                      'assets/imagen1.png',
-                    ),
-                  );
-                },
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
+          ),
+          // Ventana emergente 1
+          Positioned(
+            left: 50,
+            top: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 1',
+                    'Descripción de la ventana emergente 1',
+                    'assets/imagen1.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono1,
+                size: 50,
+                color: Colors.red,
               ),
             ),
-            // Ventana emergente 2
-            Positioned(
-              right: 50,
-              bottom: 50,
-              child: GestureDetector(
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (_) => _buildDialog(
-                      context,
-                      'Título de la ventana emergente 2',
-                      'Descripción de la ventana emergente 2',
-                      'assets/imagen2.png',
-                    ),
-                  );
-                },
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ),
+          ),
+          // Ventana emergente 2
+          Positioned(
+            right: 120,
+            bottom: 120,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
               ),
             ),
-          ],
-        ),
-        scaleEnabled: true,
-        panEnabled: true,
+          ),
+          Positioned(
+            right: 50,
+            bottom: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Positioned(
+            right: 50,
+            bottom: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Positioned(
+            right: 50,
+            bottom: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Positioned(
+            right: 50,
+            bottom: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Positioned(
+            right: 50,
+            bottom: 50,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => _buildDialog(
+                    context,
+                    'Título de la ventana emergente 2',
+                    'Descripción de la ventana emergente 2',
+                    'assets/imagen2.png',
+                  ),
+                );
+              },
+              child: Icon(
+                icono2,
+                size: 50,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
